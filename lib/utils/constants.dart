8addff71eeabc32fb00f976135d81d07a19f9fdd
wenjun <hsweib@gmail.com>
2026-02-19 06:29:52 +0800

    v1.9.5: 更新版本

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = '悦音';
-  static const String appVersion = '1.9.4';
+  static const String appVersion = '1.9.5';
   static const String appDescription = '一款简洁清新的本地音乐播放器';
   static const String defaultAlbumArt = 'assets/images/default_album_art.png';
   static const Duration animationDuration = Duration(milliseconds: 300);
